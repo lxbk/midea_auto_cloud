b2320238cba2499e5b416eedf85c7a850cc90440
Snow Zhang <aiyouv@hotmail.com>
2026-06-07 02:16:02 +0800

    Increase fan speed range from 0-9 to 0-99

--- custom_components/midea_auto_cloud/device_mapping/T0xFA.py
+++ custom_components/midea_auto_cloud/device_mapping/T0xFA.py
@@ -24,7 +24,7 @@ DEVICE_MAPPING = {
             Platform.FAN: {
                 "fan": {
                     "power": "power",
-                    "speeds": list({"gear": value + 1} for value in range(0, 9)),
+                    "speeds": list({"gear": value + 1} for value in range(0, 99)),
                     "oscillate": "swing",
                     "preset_modes": {
                         "normal": {"mode": "normal"},
